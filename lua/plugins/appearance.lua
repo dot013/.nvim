a905b3273791e590a530b1eda038a573b5a0494a
Guz013 <contact.guz013@gmail.com>
2023-12-30 06:01:03 -0300

    feat: catppuccin theme

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -1,19 +1,35 @@
 -- Plugins related to the colorscheme and Neovim appearence
-return { {
+return { --[[ {
 	'shaunsingh/nord.nvim',
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme('nord');
-		vim.g.nord_disable_background = true;
+		if not vim.g.startted_by_firenvim then
+			vim.g.nord_disable_background = true;
+		else
+			vim.g.nord_disable_background = false;
+		end
 
 		require('nord').set();
 	end,
-}, {
+}, ]] --
+	{
+		'catppuccin/nvim',
+		name = 'catppuccin',
+		priority = 100,
+		config = function()
+			require('catppuccin').setup({
+				flavour = 'mocha',
+				transparent_background = true,
+			});
+			vim.cmd.colorscheme('catppuccin');
+		end
+	}, {
 	'nvim-lualine/lualine.nvim',
 	opts = {
 		options = {
 			icons_enabled = false,
-			theme = 'nord',
+			theme = 'catppuccin',
 			component_separators = '|',
 			section_separators = '',
 		},
