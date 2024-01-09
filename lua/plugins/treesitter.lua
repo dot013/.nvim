c30072ea9b856fdc3ed14c254350259d504e2626
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-01-09 19:28:13 -0300

    feat: install treesitter playground

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return { {
 	'nvim-treesitter/nvim-treesitter',
 	dependencies = {
 		'nvim-treesitter/nvim-treesitter-textobjects',
+		'nvim-treesitter/playground',
 	},
 	build = ':TSUpdate',
 	config = function()
@@ -62,6 +63,29 @@ return { {
 					swap_previous = {
 						['<leader>A'] = '@parameter.inner',
 					},
+				},
+				playground = {
+					enable = true,
+					disable = {},
+					updatetime = 25,
+					persist_queries = false,
+					keybindings = {
+						toggle_query_editor = 'o',
+						toggle_hl_groups = 'i',
+						toggle_injected_languages = 't',
+						toggle_anonymous_nodes = 'a',
+						toggle_language_display = 'I',
+						focus_language = 'f',
+						unfocus_language = 'F',
+						update = 'R',
+						goto_node = '<cr>',
+						show_help = '?',
+					},
+				},
+				query_linter = {
+					enable = true,
+					use_virtual_text = true,
+					lint_events = {'BugWrite', 'CursorHold'},
 				},
 			})
 		end, 0);
