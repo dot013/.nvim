6f56766ba108ca46478da6800753c3066fcbabbb
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-03-20 15:59:39 -0300

    feat: add neo-tree

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -126,6 +126,45 @@ return {
 		lazy = false,
 	},
 	{
+		'nvim-neo-tree/neo-tree.nvim',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'nvim-tree/nvim-web-devicons',
+			'MunifTanjim/nui.nvim',
+			'3rd/image.nvim',
+		},
+		lazy = false,
+		opts = {
+			closes_if_last_window = true,
+			window = {
+				position = 'left',
+				width = 30,
+				mappings = {},
+			},
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_by_name = {
+					},
+					never_show = {
+						'.DS_Store',
+					},
+				},
+				hijack_netrw_behavior = 'disabled',
+				follow_current_file = {
+					enabled = true,
+				},
+				window = { mappings = {} },
+			},
+		},
+		keys = {
+			{ '<leader>Ex', '<cmd>Neotree toggle<cr>', desc = '[Ex] Toggle explorer' },
+		},
+
+	},
+	{
 		'stevearc/conform.nvim',
 		opts = {
 			formatters_by_ft = {
