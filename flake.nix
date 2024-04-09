39b907400d1d596fed0d57411b7512d69b2644f6
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-04-09 12:10:44 -0300

    feat: flake.nix for working in the neovim config with direnv

--- flake.nix
+++ flake.nix
@@ -1,51 +1,26 @@
 {
-  description = "My NeoVim config";
+  description = "learning.rs";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    neovim = {
-      url = "github:neovim/neovim/stable?dir=contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
-
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, neovim, flake-utils, gen-luarc, ... }@inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      neovim-overlay = import ./overlay.nix { inherit inputs; };
+      pkgs = import nixpkgs {
+        inherit system;
+      };
     in
-    flake-utils.lib.eachSystem supportedSystems
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              neovim-overlay
-              gen-luarc.overlays.default
-            ];
-          };
-          shell = pkgs.mkShell {
-            name = "nvim-devShell";
-            builtInputs = with pkgs; [
-              lua-language-server
-              nil
-              stylua
-              luajitPackages.luacheck
-              deno
-            ];
-            shellHook = ''
-              ln -fs ${pkgs.nvim-luarc-json} .luarc.json
-            '';
-          };
-        in
-        {
-          packages = rec { default = nvim; nvim = pkgs.nvim-pkg; };
-          devShells = { default = shell; };
-        }) // { overlays.default = neovim-overlay; };
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          stylua
+        ];
+      };
+    });
 }
